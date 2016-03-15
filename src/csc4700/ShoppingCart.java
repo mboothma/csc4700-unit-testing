e05927daa02c08a7b19f296f4c81d4d6c1dd64ad
Jay Dobies <jason.dobies@redhat.com>
2016-03-14 20:58:36 -0400

    Reworked project for spr-2016

---
+++ src/csc4700/ShoppingCart.java
@@ -0,0 +1,54 @@
+package csc4700;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    private List<CartItem> cartItems = new ArrayList<CartItem>();
+
+    public void addItem(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+
+        CartItem cartItem = findCartItem(item);
+        if (cartItem == null) {
+            cartItem = new CartItem(item);
+            cartItems.add(cartItem);
+        }
+
+        cartItem.incrementCountByOne();
+    }
+
+    public void deleteItem(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+
+        CartItem cartItem = findCartItem(item);
+        if (cartItem == null) {
+            return;
+        }
+
+        if (cartItem.getCount() == 0) {
+            cartItems.remove(cartItem);
+        }
+        else {
+            cartItem.decrementCountByOne();
+        }
+    }
+
+    public CartItem findCartItem(Item item) {
+        for (CartItem i : cartItems) {
+            if (i.getItem().equals(item)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+}
