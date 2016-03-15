895ab7efae9e38bb0a23f6f81138cc764ecba9d2
Jay Dobies <jason.dobies@redhat.com>
2016-03-15 18:33:42 -0400

    Added comments for clarity

--- src/csc4700/ShoppingCart.java
+++ src/csc4700/ShoppingCart.java
@@ -13,11 +13,16 @@ public class ShoppingCart {
         }
 
         CartItem cartItem = findCartItem(item);
+
+        // If there is no CartItem, this is the first time it is being
+        // added to the cart and we need to create the mapping object
         if (cartItem == null) {
             cartItem = new CartItem(item);
             cartItems.add(cartItem);
         }
 
+        // Items may only be added one at a time in this implementation,
+        // so simply increment the quantity.
         cartItem.incrementCountByOne();
     }
 
@@ -27,6 +32,11 @@ public class ShoppingCart {
         }
 
         CartItem cartItem = findCartItem(item);
+
+        // If there is no corresponding CartItem object, there is nothing
+        // to delete. Some implementations will choose to throw an exception
+        // in this case, however for this project we'll simply exit (having
+        // met the end goal of not having the item in the cart).
         if (cartItem == null) {
             return;
         }
@@ -37,6 +47,8 @@ public class ShoppingCart {
             cartItems.remove(cartItem);
         }
         else {
+            // Only decrement the quantity by one, but since there will still
+            // be at least one of the item desired, leave the CartItem in place.
             cartItem.decrementCountByOne();
         }
     }
